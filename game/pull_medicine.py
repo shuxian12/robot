64974b516213566e89b95a6b71e92270104a20a2
Jack-1D <10635052@mail.hpsh.tp.edu.tw>
2023-05-24 13:02:47 +0800

    add game directory

---
+++ game/pull_medicine.py
@@ -0,0 +1,156 @@
+import pygame
+import numpy as np
+import random
+import math
+
+pygame.init()
+window_width = 800
+window_height = 600
+
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("資安中藥事件")
+
+clock = pygame.time.Clock()
+
+number = random.randint(1,500000)
+number = np.round(number/100,2)
+
+def setup():
+    window.blit(pygame.transform.scale(pygame.image.load('./medicine/background.jpg'),(800,600)),(0,0))
+    window.blit(pygame.transform.scale(pygame.image.load('./medicine/scale.png'),(300,300)),(140,250))
+    window.blit(pygame.transform.scale(pygame.image.load('./medicine/pull.png'),(176,71)),(500,500))
+    window.blit(pygame.transform.scale(pygame.image.load('./medicine/clear.png'),(176,71)),(100,500))
+    pygame.draw.rect(window,(255,255,255),pygame.Rect(465,463,244,40))
+    window.blit(pygame.font.Font('./fonts/corbel.ttf',24).render(f'Need number: {number}',True,(0,0,0)),(475,470))
+
+num_medicine = [200,350,450,1000]
+num_posi = [(325,393),(310,393),(285,393),(270,393),(255,393),(240,393)]
+flow = [0,0,0,0,0,0,0,0,0,0.03,0.178,0.355,0.726,1.42,3.87]
+size = [(282,156),(253.8,140.4),(253.8,140.4),(253.8,140.4),(253.8,140.4),(275,145),(253.8,140.4),(260,145),\
+        (274,150),(280,160),(275,160),(280,160),(270,166),(265,164),(278,170)]
+posi = [(380,100),(395,120),(395,120),(395,120),(395,120),(380,127),(395,130),(395,127),(380,130),(380,127),\
+        (380,130),(380,130),(380,130),(380,135),(376,135)]
+weight = 0
+while True:
+    stop = False
+    setup()
+    window.blit(pygame.transform.scale(pygame.image.load('./medicine/1.png'),(282,156)),(380,100))
+    
+    # 磅秤上放藥
+    if int(weight) >= num_medicine[0]:
+        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,320))
+    if int(weight) >= num_medicine[1]:
+        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,310))
+    if int(weight) >= num_medicine[2]:
+        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,300))
+    if int(weight) >= num_medicine[3]:
+        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,290))
+    
+
+    pygame.draw.rect(window,(240,240,240),pygame.Rect(270,391,70,28))
+    pygame.draw.rect(window,(0,0,0),pygame.Rect(305,412,3,3))
+    if weight <= 0.01:
+        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(285,393))
+        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(310,393))
+        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(325,393))
+    else:
+        tmp = weight*100
+        index = 0
+        while int(tmp) != 0:
+            window.blit(pygame.transform.scale(pygame.image.load(f'./medicine/num{int(tmp%10)}.png'),(14,23)),num_posi[index])
+            index += 1
+            tmp//=10
+        if weight < 0.01:
+            window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(325,393))
+        if weight < 0.1:
+            window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(310,393))
+        if weight < 1:
+            window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(285,393))
+
+    for event in pygame.event.get():
+        # print(pygame.mouse.get_pos())
+        if event.type == pygame.QUIT:
+            exit()
+        i = 0
+        if event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+            x = pygame.mouse.get_pos()[0]
+            y = pygame.mouse.get_pos()[1]
+            if x>=505 and x<=669 and y>=503 and y<=564:
+                while not stop:
+                    for event in pygame.event.get():
+                        if event.type == pygame.MOUSEBUTTONUP:
+                            stop = True
+                    setup()
+                    window.blit(pygame.transform.scale(pygame.image.load(f'./medicine/{i+1}.png'),size[i]),posi[i])
+                    pygame.draw.rect(window,(240,240,240),pygame.Rect(270,391,70,28))
+                    pygame.draw.rect(window,(0,0,0),pygame.Rect(305,412,3,3))
+
+                    if int(weight) >= num_medicine[0]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,320))
+                    if int(weight) >= num_medicine[1]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,310))
+                    if int(weight) >= num_medicine[2]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,300))
+                    if int(weight) >= num_medicine[3]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,290))
+
+                    weight += flow[i]
+                    weight = np.round(weight,2)
+                    weight = 9999.99 if weight>=9999.99 else weight
+                    tmp = weight*100
+                    index = 0
+                    while int(tmp) != 0:
+                        window.blit(pygame.transform.scale(pygame.image.load(f'./medicine/num{int(tmp%10)}.png'),(14,23)),num_posi[index])
+                        index += 1
+                        tmp//=10
+                    if weight < 0.01:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(325,393))
+                    if weight < 0.1:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(310,393))
+                    if weight < 1:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(285,393))
+                    clock.tick(20)
+                    pygame.display.flip()
+                    i = i+1 if i<14 else 14
+                while i >= 0:
+                    setup()
+                    window.blit(pygame.transform.scale(pygame.image.load(f'./medicine/{i+1}.png'),size[i]),posi[i])
+                    pygame.draw.rect(window,(240,240,240),pygame.Rect(270,391,70,28))
+                    pygame.draw.rect(window,(0,0,0),pygame.Rect(305,412,3,3))
+
+                    if int(weight) >= num_medicine[0]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,320))
+                    if int(weight) >= num_medicine[1]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,310))
+                    if int(weight) >= num_medicine[2]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,300))
+                    if int(weight) >= num_medicine[3]:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/medicine.png'),(250,30)),(170,290))
+
+                    weight += flow[i]
+                    weight = np.round(weight,2)
+                    weight = 9999.99 if weight>=9999.99 else weight
+                    tmp = weight*100
+                    index = 0
+                    while int(tmp) != 0:
+                        window.blit(pygame.transform.scale(pygame.image.load(f'./medicine/num{int(tmp%10)}.png'),(14,23)),num_posi[index])
+                        index += 1
+                        tmp//=10
+                    if weight < 0.01:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(325,393))
+                    if weight < 0.1:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(310,393))
+                    if weight < 1:
+                        window.blit(pygame.transform.scale(pygame.image.load('./medicine/num0.png'),(14,23)),(285,393))
+                    clock.tick(20)
+                    pygame.display.flip()
+                    i-=1
+                if abs(weight-number) <= 10:
+                    exit()
+
+            if x>=107 and x<=270 and y>=503 and y<=564:
+                weight = 0
+        
+    clock.tick(60)
+    pygame.display.flip()
+pygame.quit()
---
+++ game/shoot.py
@@ -0,0 +1,108 @@
+import pygame
+import random
+import math
+
+color = ['clubs','diamonds','hearts','spades']
+number = ['ace','2','3','4','5','6','7','8','9','10','jack','queen','king']
+left_symbol_posi_x = 80
+left_symbol_posi_y = 80
+right_symbol_posi_x = 650
+right_symbol_posi_y = 80
+my_symbol_posi_x = 365
+my_symbol_posi_y = 200
+card_images = []
+for n in number:
+    for c in color:
+        image = pygame.transform.scale(pygame.image.load(f'./cards/{n}_of_{c}.png'),(150,219))
+        card_images.append(image)
+
+
+pygame.init()
+window_width = 800
+window_height = 600
+
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("射龍門")
+
+clock = pygame.time.Clock()
+
+def create_rand():
+    rand1 = random.randint(0,51)
+    rand2 = random.randint(0,51)
+    my_rand = random.randint(0,51)
+    while rand1 == rand2 or rand2 == my_rand or my_rand == rand1:
+        rand2 = random.randint(0,51)
+        my_rand = random.randint(0,51)
+    if(rand1 > rand2):
+        rand1, rand2 = rand2, rand1
+    return rand1, my_rand, rand2
+
+def judge(rand1, my_rand, rand2):
+    if my_rand//4 < rand1//4 or my_rand//4 > rand2//4:
+        return 'Lose'
+    elif my_rand//4 == rand1//4 or my_rand//4 == rand2//4:
+        return "Hit"
+    else:
+        return "Win"
+
+start = False
+show_my_card = False
+bid = 0
+while True:    
+    for event in pygame.event.get():
+        # print(pygame.mouse.get_pos())
+        if event.type == pygame.QUIT:
+            exit()
+        window.blit(pygame.transform.scale(pygame.image.load('./cards/background.png'),(800,600)),(0,0))
+        pygame.draw.rect(window,(36,132,58),pygame.Rect(16,6,112,151))
+        pygame.draw.rect(window,(40,149,66),pygame.Rect(16,157,760,230))
+        window.blit(pygame.transform.scale(pygame.image.load(f'./cards/back.jpg'),(150,219)),(60,60))
+        window.blit(pygame.transform.scale(pygame.image.load(f'./cards/back.jpg'),(150,219)),(320,60))
+        window.blit(pygame.transform.scale(pygame.image.load(f'./cards/back.jpg'),(150,219)),(580,60))
+        pygame.draw.rect(window,(200,20,20),pygame.Rect(320,500,150,50))
+        window.blit(pygame.font.Font('./fonts/corbel.ttf',35).render('Start',True,(20,20,20)),(358,510))
+        window.blit(pygame.font.Font('./fonts/corbel.ttf',35).render(f'{bid}',True,(255,255,255)),(320,400))
+        window.blit(pygame.transform.scale(pygame.image.load(f'./cards/minus.png'),(40,40)),(260,400))
+        window.blit(pygame.transform.scale(pygame.image.load(f'./cards/plus.png'),(40,40)),(500,400))
+        window.blit(pygame.transform.scale(pygame.image.load(f'./cards/screw.png'),(60,60)),(560,395))
+        # 按下start
+        if not start and event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+            posi = pygame.mouse.get_pos()
+            if posi[0]<=470 and posi[0]>=320 and posi[1]<=550 and posi[1]>=500:
+                start = True
+                rand1, my_rand, rand2 = create_rand()
+        # 按下restart
+        if start and show_my_card and event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+            posi = pygame.mouse.get_pos()
+            if posi[0]<=470 and posi[0]>=320 and posi[1]<=550 and posi[1]>=500:
+                start = False
+                show_my_card = False
+        # 按下住
+        if not start and event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+            posi = pygame.mouse.get_pos()
+            if math.sqrt(math.pow(posi[0]-280,2)+math.pow(posi[1]-420,2)) <= 19:
+                bid = 0 if bid == 0 else bid-100
+            if math.sqrt(math.pow(posi[0]-520,2)+math.pow(posi[1]-420,2)) <= 19:
+                bid = 1000000 if bid == 1000000 else bid+100
+        # 按我的牌
+        if start and event.type == pygame.MOUSEBUTTONDOWN and pygame.mouse.get_pressed()[0]:
+            posi = pygame.mouse.get_pos()
+            if posi[0]<=470 and posi[0]>=320 and posi[1]<=279 and posi[1]>=60:
+                show_my_card = True
+                if judge(rand1, my_rand, rand2) == 'Win':
+                    print("Win")
+                elif judge(rand1, my_rand, rand2) == 'Lose':
+                    print("Lose")
+                else:
+                    print("Hit")
+
+        if start:
+            window.blit(card_images[rand1],(60,60))
+            window.blit(card_images[rand2],(580,60))
+        if show_my_card:
+            window.blit(card_images[my_rand],(320,60))
+            pygame.draw.rect(window,(200,20,20),pygame.Rect(320,500,150,50))
+            window.blit(pygame.font.Font('./fonts/corbel.ttf',35).render('Restart',True,(20,20,20)),(340,510))
+        clock.tick(60)
+        pygame.display.flip()
+pygame.quit()
